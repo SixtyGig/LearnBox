8ad66d0132b4a444f92973af788870f175a83544
LAlemanyT <luisalemany@outlook.com>
2020-09-20 20:50:16 +1000

    trash script

---
+++ Assets/Scenes/Beach/TrashInBin.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashInBin : MonoBehaviour
+{
+    public int trashPicked;
+    public int trashTotal;
+    private GameObject manager;
+    private ScenarioManager scene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = GameObject.FindGameObjectWithTag("ScenarioManager");
+        scene = manager.GetComponent<ScenarioManager>();
+        trashPicked = 0;
+        trashTotal = 8;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (trashPicked >= trashTotal)
+        {
+            scene.tasklist["PickUpTrash"] = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        trashPicked++;
+        Destroy(other.gameObject);
+    }
+}
