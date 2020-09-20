8b37cdbb6b49cb8d145e958ae20d6251a7bbe649
SixtyGig <38166922+SixtyGig@users.noreply.github.com>
2020-09-20 05:23:26 -0500

    Updates

---
+++ Assets/Whack-A-Mole/Hammer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hammer : MonoBehaviour
+{
+    GameObject SM;
+    // Start is called before the first frame update
+
+
+
+}
---
+++ Assets/Whack-A-Mole/HammerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HammerController : MonoBehaviour
+{
+    public Text scoreText;
+    public int score;
+    private MoleSpawn ms;
+    private GameObject SM;
+    float range = 10f;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        ms = GetComponent<MoleSpawn>();
+        SM = GameObject.FindGameObjectWithTag("ScenarioManager");
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("hit");
+        Destroy(other);
+        score = score + 1;
+        scoreText.text = score.ToString();
+        ms.spawn();
+    }
+}
